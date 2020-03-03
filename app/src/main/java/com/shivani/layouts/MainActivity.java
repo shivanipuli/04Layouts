5a0a7733a7b7d0444ac3392cc7ab2cf2294aa471
Shivani <shivanipuli@gmail.com>
2020-03-03 17:30:07 -0500

    Finished Layout

--- app/src/main/java/com/shivani/layouts/MainActivity.java
+++ app/src/main/java/com/shivani/layouts/MainActivity.java
@@ -3,22 +3,27 @@ package com.shivani.layouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    int[] layouts = {R.layout.activity_main,R.layout.activity_1, R.layout.activity_2, R.layout.activity_3, R.layout.activity_4, R.layout.activity_6};
+    int[] layouts = {R.layout.activity_main,R.layout.activity_7, R.layout.activity_2, R.layout.activity_3, R.layout.activity_4,R.layout.activity_7,R.layout.activity_1};
     int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layouts[count]);
     }
 
     public void next(View view) {
         setContentView(layouts[count]);
-        count += 1;
-        if(count > 6) count = 0;
+        count++;
+        if(count>=layouts.length){
+            count=0;
+        }
+
+        Log.i("next", "count=" + count);
     }
 }
