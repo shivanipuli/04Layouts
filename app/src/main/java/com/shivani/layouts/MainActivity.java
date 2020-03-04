f5ec9c086f38d56142aada3ef5d56a99ee0177ea
Shivani <shivanipuli@gmail.com>
2020-03-04 13:03:58 -0500

    Checkboxes in different xml file

--- app/src/main/java/com/shivani/layouts/MainActivity.java
+++ app/src/main/java/com/shivani/layouts/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    int[] layouts = {R.layout.activity_main,R.layout.activity_7, R.layout.activity_2, R.layout.activity_3, R.layout.activity_4,R.layout.activity_7,R.layout.activity_1};
+    int[] layouts = {R.layout.activity_main,R.layout.activity_6, R.layout.activity_2, R.layout.activity_3, R.layout.activity_4,R.layout.activity_7,R.layout.activity_1};
     int count = 1;
 
     @Override
